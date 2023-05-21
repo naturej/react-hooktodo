d42e492b09e8b7eb057f537753e921be5ec4bd6b
naturej <natu_rej@naver.com>
2023-05-21 21:13:45 +0900

    feat: cracro 라이브러리 설치 및 세팅

---
+++ craco.config.js
@@ -0,0 +1,12 @@
+const path = require("path");
+
+module.exports = {
+  webpack: {
+    alias: {
+      "@": path.resolve(__dirname, "src/"),
+      "@components": path.resolve(__dirname, "src/components"),
+      "@hooks": path.resolve(__dirname, "src/hooks"),
+      "@pages": path.resolve(__dirname, "src/pages"),
+    },
+  },
+};
