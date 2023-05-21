a997374d4db738264fe9aacb105e5a260db1efb4
naturej <natu_rej@naver.com>
2023-05-21 21:50:09 +0900

    feat: styles 경로 추가

--- craco.config.js
+++ craco.config.js
@@ -3,10 +3,10 @@ const path = require("path");
 module.exports = {
   webpack: {
     alias: {
-      "@": path.resolve(__dirname, "src/"),
       "@components": path.resolve(__dirname, "src/components"),
       "@hooks": path.resolve(__dirname, "src/hooks"),
       "@pages": path.resolve(__dirname, "src/pages"),
+      "@styles": path.resolve(__dirname, "src/styles"),
     },
   },
 };
